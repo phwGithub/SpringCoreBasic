b356463fd2c4734a1fadb7ab0f5b734bf3ddab6e
phwGithub <yey6689@gmail.com>
2023-06-05 21:02:07 +0900

    스프링 컨테이너 생성

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -18,6 +18,11 @@ public class OrderApp {
         // OrderService orderService = appConfig.orderService();
 
         // 스프링으로 전환 => 기존 AppConfig 설정 정보를 기반으로 스프링 컨테이너 생성
+        // AppConfig에 @Bean 태그가 있는 메소드들을 호출하여 객체 생성, 이름은 기본적으로 메소드의 이름을 따라간다
+        // Bean의 이름은 무조건 고유해야 한다 => 아닐 시 에러, 현업에서는 명확하게 다른 이름을 써야 함
+        // 그 후 만들어진 Bean 사이의 의존관계를 주입한다
+        // 현재 만들어진 AppConfig는 호출하면서 다른 Bean도 생성되어 생성 -> 주입 이라는 단계가 명확하게 나누어지지 않는다
+        // 단계를 나누는 것이 어떤 면에서 유리할까?
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
         // @Bean으로 등록된 객체(Bean)들을 스프링 컨테이너에서 가져오기
