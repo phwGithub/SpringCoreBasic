fa499d35f38b99b7f5038f41e9ec50d510924e08
phwGithub <yey6689@gmail.com>
2023-06-05 20:26:15 +0900

    스프링으로 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,27 +9,38 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 구현체의 생성과 주입을 전문으로 하는 클래스
 // 여기서 만들어진 구현체를 클라이언트(MemberServiceImpl, OrderServiceImpl)에 생성자를 통해서 주입한다
 // 어떤 구현체를 주입할지는 여기서 결정한다 => 관심사의 분리
+
+@Configuration // 스프링 전환을 위한 선언
 public class AppConfig {
-    
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
     // 역할을 명확하게 하기 위해 memberService() 에서 분리
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     // 역할을 명확하게 하기 위해 orderService() 에서 분리
+    @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        // 할인 정책이 변경됐을 시 이 부분만 변경하면 된다 => 배우를 교체하는 곳
+        // 클라이언트 코드를 변경할 필요가 없어졌다
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,13 +7,24 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+        // OrderService orderService = appConfig.orderService();
+
+        // 스프링으로 전환 => 기존 AppConfig 설정 정보를 기반으로 스프링 컨테이너 생성
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // @Bean으로 등록된 객체(Bean)들을 스프링 컨테이너에서 가져오기
+        // 이름은 디폴트로 객체 생성 메소드의 이름을 따라감
+        // 이 방식이 기존 자바 코드보다 뭐가 더 좋을까?
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
