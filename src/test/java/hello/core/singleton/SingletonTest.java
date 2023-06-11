616603f8d7711e7d7aad04387c4bbe86490db931
phwGithub <yey6689@gmail.com>
2023-06-11 21:24:55 +0900

    웹 애플리케이션과 싱글톤

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,32 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        // 1. 조회 : 호출할 때마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        // 2. 조회 : 호출할 때마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        // 조회할 때마다 참조값이 다른 인스턴스가 생성된다
+        // 조회량이 많다면 그만큼의 객체가 생김 => 문제
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
