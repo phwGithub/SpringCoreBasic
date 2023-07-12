ab9326bd88d3f1c3629b0b4072abbe15836cd8f8
phwGithub <yey6689@gmail.com>
2023-07-12 13:23:59 +0900

    인터페이스를 이용한 초기화 및 소멸 콜백

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,46 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작 시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    // 서비스 종료 시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    // 의존관계 주입이 끝난 후 실행되는 콜백 함수
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 빈이 종료될 때 호출되는 콜백 함수
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+}
