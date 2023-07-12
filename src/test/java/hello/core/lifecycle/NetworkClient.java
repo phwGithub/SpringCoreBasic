b3bf1a722babd5b00831696afd100b9ac896e8f1
phwGithub <yey6689@gmail.com>
2023-07-12 13:33:13 +0900

    설정 정보에서 초기화 및 소멸 메서드 지정

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -30,17 +27,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     // 의존관계 주입이 끝난 후 실행되는 콜백 함수
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
     // 빈이 종료될 때 호출되는 콜백 함수
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
