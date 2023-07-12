538c4ed2356e6a093dfc392513da96795b7f2f6c
phwGithub <yey6689@gmail.com>
2023-07-12 22:26:44 +0900

    request 스코프 예제

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,31 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    // 이대로 스프링을 실행하면 오류가 남 왜 why? myLogger의 스코프가 request라 요청이 없으면 빈이 잡히지 않는다
+    // Provider의 DL 기능을 통해 해결 가능하다
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestUrl = request.getRequestURL().toString();
+        myLogger.setRequestUrl(requestUrl);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+
+        return "OK";
+    }
+}
